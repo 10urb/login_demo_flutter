f9f2120fe7e9d9cfa651b401b350c232bfb98ac8
Onur Bulut <10urbulut@gmail.com>
2022-03-23 03:34:32 +0300

    Initial

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_demo_flutter/business/weather_manager.dart';
 import 'package:login_demo_flutter/constants/strings/title_strings.dart';
+import 'package:login_demo_flutter/models/weather_model/weather_model.dart';
 import 'package:provider/provider.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -12,25 +13,37 @@ class WeatherScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text(TitleStrings.WEATHER_STATE),
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.symmetric(
-            vertical: MediaQuery.of(context).size.height / 80,
-            horizontal: MediaQuery.of(context).size.width / 50),
-        itemCount: context.watch<WeatherManager>().weathers.length,
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, index) {
-          var weather = context.read<WeatherManager>().weathers[index];
-          return ListTile(
-            leading: Icon(
-              Icons.power_input_rounded,
-              color: weather.degree! < 15 ? Colors.blue : Colors.orange,
-            ),
-            title: Text('Degree: ${weather.degree}'),
-            subtitle: Text(
-                'Date: ${weather.dayOfMonth}/${weather.month} ${weather.dayName}'),
-          );
-        },
-      ),
+      body: _body(context),
     );
+  }
+
+
+
+  ListView _body(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(
+          vertical: MediaQuery.of(context).size.height / 80,
+          horizontal: MediaQuery.of(context).size.width / 50),
+      itemCount: context.watch<WeatherManager>().weathers.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        var weather = context.read<WeatherManager>().weathers[index];
+        return _listItem(weather);
+      },
+    );
+  }
+
+  
+
+  ListTile _listItem(WeatherModel weather) {
+    return ListTile(
+          leading: Icon(
+            Icons.power_input_rounded,
+            color: weather.degree! < 15 ? Colors.blue : Colors.orange,
+          ),
+          title: Text('Degree: ${weather.degree}'),
+          subtitle: Text(
+              'Date: ${weather.dayOfMonth}/${weather.month} ${weather.dayName}'),
+        );
   }
 }
